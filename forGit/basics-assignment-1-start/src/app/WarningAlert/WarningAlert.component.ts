07c91a68fc2c43f80e9af9058d9a40fec7229260
Byron <v.beleris@outlook.com>
2017-08-02 10:41:51 +0100

    2nd commit. basics-assingment done. my-first-app in progress

---
+++ forGit/basics-assignment-1-start/src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-WarningAlert',
+  templateUrl: './WarningAlert.component.html',
+  styleUrls: ['./WarningAlert.component.css']
+})
+export class WarningAlert{
+
+}
