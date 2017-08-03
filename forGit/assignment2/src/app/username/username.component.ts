24414ecc2c7d5c4cebd4ef5619d4ce47dadac858
Byron <v.beleris@outlook.com>
2017-08-03 13:02:25 +0100

    assignment 2 ready.

--- forGit/assignment2/src/app/username/username.component.ts
+++ forGit/assignment2/src/app/username/username.component.ts
@@ -6,25 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit {
-  usernameText = '';
-  allowNewUsername = false;
-  constructor() {
-   }
-
-
-  ngOnInit() {
-
-  }
-  activeButton(){
-    if (this.usernameText !=""){
-      this.allowNewUsername=true;
+    usernameText = '';
+    allowNewUsername = false;
+    namesList = [];
+    toggle = false;
+    constructor() {
     }
-
-  }
-  onClearUsername(){
-    this.usernameText = "";
-    this.allowNewUsername = false;
+    ngOnInit() {
     }
-
-
+    activeButton(){
+        if (this.usernameText != ""){
+            this.allowNewUsername=true;
+        }
+        
+    }
+    onAddUsername(){
+        this.namesList.push(this.usernameText);
+        if (this.namesList != []){
+            this.toggle = true;
+        }
+        this.usernameText = "";
+        this.allowNewUsername = false;
+        }
+    onClearUsernameList(){ 
+        this.namesList = [];
+        this.toggle = false;
+    }
   }
