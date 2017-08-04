14ff7ca21cbc96a90d7d0dd6394ec46766294252
Byron <v.beleris@outlook.com>
2017-08-04 17:41:03 +0100

    in progress

---
+++ forGit/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated  //None, Native
+})
+export class ServerElementComponent implements OnInit {
+  // @Input() element: { type: string , name: string , content: string };
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: { type: string , name: string , content: string };
+  constructor() {
+    console.log('constructor called!');
+   }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+}
