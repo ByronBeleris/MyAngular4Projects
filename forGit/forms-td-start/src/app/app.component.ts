34db6517a3088466f5a8b7767de5cf46037152b7
ByronBeleris <v.beleris@outlook.com>
2017-08-11 14:35:21 +0100

    done

--- forGit/forms-td-start/src/app/app.component.ts
+++ forGit/forms-td-start/src/app/app.component.ts
@@ -1,26 +1,55 @@
-import { NgForm } from '@angular/forms';
 import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
+export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = 'pet';
+  defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
+
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
-  // This is the best way to retreive username etc.
-  // But you can also do it with @Viewchild
+
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
