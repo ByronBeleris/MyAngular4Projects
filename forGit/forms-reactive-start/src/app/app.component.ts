a91f2126075739b96efc2a5f85d8087b37033cc6
ByronBeleris <v.beleris@outlook.com>
2017-08-11 20:15:50 +0100

    in progress

---
+++ forGit/forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    // this.signupForm = new FormGroup({
+    //   'username': new FormControl(null, Validators.required),
+    //   'email': new FormControl(null, [Validators.required, Validators.email]),
+    //   'gender': new FormControl('male')
+    //     });
+    // For this remove <div formGroupName="userData" and .get(userData.username) only userData. from all. 
+
+
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])  
+      }),
+      'gender': new FormControl('male')      
+    });
+      }  
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
