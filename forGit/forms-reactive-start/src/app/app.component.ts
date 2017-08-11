a065b8b8c4b60a6f4fc9c3b908f5ed93aaba4b12
ByronBeleris <v.beleris@outlook.com>
2017-08-11 21:54:15 +0100

    done

--- forGit/forms-reactive-start/src/app/app.component.ts
+++ forGit/forms-reactive-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Byron', 'Sylvia'];
 
   ngOnInit() {
     // this.signupForm = new FormGroup({
@@ -21,13 +23,68 @@ export class AppComponent implements OnInit {
 
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email])  
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)  
       }),
-      'gender': new FormControl('male')      
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])   
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+    this.signupForm.setValue({
+      'userData':{
+        'username': 'Me',
+        'email': 'b@vb.b'
+      },
+      'gender':'male',
+      'hobbies': []
+      });
+    this.signupForm.patchValue({
+      'userData':{
+        'username': 'You'
+      }
+      });
       }  
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
+    // this.signupForm.reset({
+    //   'userData':{
+    //     'username': '',
+    //     'email': ''
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    //   });
+    // or this to reset to a specific object
+    }
+  onAddHobby(){
+      const control = new FormControl(null, Validators.required);
+      (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl):{[s: string]: boolean}{
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1 ){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+  // this is custom validator (lecture 195)
+
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        }
+        resolve(null);
+      }, 1500);
+    } );
+    return promise;
   }
 }
