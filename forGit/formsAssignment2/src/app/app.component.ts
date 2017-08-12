53819af51354a95f6a5a7489d700b05eadb90f77
ByronBeleris <v.beleris@outlook.com>
2017-08-12 13:11:56 +0100

    done

---
+++ forGit/formsAssignment2/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  // defStatus= 'stable';
+  statusList = ['stable', 'critical', 'finished'];
+  statusForm: FormGroup;
+  forbiddenProjectNames = ['Test'];
+
+  ngOnInit() {
+    this.statusForm = new FormGroup({
+      'projectData': new FormGroup({
+        'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'status': new FormControl('stable')
+    });
+  }
+
+
+
+  onSubmit(){
+    console.log(this.statusForm);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenProjectNames.indexOf(control.value) !== -1 ) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+}
